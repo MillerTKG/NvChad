dc8989de64a52a888f2c0ec87968b6a9472e9282
MillerTKG <cjrdm.mt@gmail.com>
2025-06-07 13:22:03 -0400

    whichkey changes

--- lua/nvchad/plugins/which-key.lua
+++ lua/nvchad/plugins/which-key.lua
@@ -1,5 +1,6 @@
 return {
   "folke/which-key.nvim",
+  event = "VimEnter",
   -- keys = { "<leader>", "<c-w>", '"', "'", "`", "c", "v", "g" },
   cmd = "WhichKey",
   opts = {
@@ -7,9 +8,57 @@ return {
       dofile(vim.g.base46_cache .. "whichkey")
       return {}
     end,
+    delay = 0,
+    icons = {
+      -- set icon mappings to true if you have a Nerd Font
+      mappings = vim.g.have_nerd_font,
+      -- If you are using a Nerd Font: set icons.keys to an empty table which will use the
+      -- default which-key.nvim defined Nerd Font icons, otherwise define a string table
+      keys = vim.g.have_nerd_font and {} or {
+        Up = "<Up> ",
+        Down = "<Down> ",
+        Left = "<Left> ",
+        Right = "<Right> ",
+        C = "<C-…> ",
+        M = "<M-…> ",
+        D = "<D-…> ",
+        S = "<S-…> ",
+        CR = "<CR> ",
+        Esc = "<Esc> ",
+        ScrollWheelDown = "<ScrollWheelDown> ",
+        ScrollWheelUp = "<ScrollWheelUp> ",
+        NL = "<NL> ",
+        BS = "<BS> ",
+        Space = "<Space> ",
+        Tab = "<Tab> ",
+        F1 = "<F1>",
+        F2 = "<F2>",
+        F3 = "<F3>",
+        F4 = "<F4>",
+        F5 = "<F5>",
+        F6 = "<F6>",
+        F7 = "<F7>",
+        F8 = "<F8>",
+        F9 = "<F9>",
+        F10 = "<F10>",
+        F11 = "<F11>",
+        F12 = "<F12>",
+      },
+    },
     triggers = {
       { "<auto>", mode = "nixsotc" },
       { "s", mode = { "n", "v" } },
+    },
+    spec = {
+      { "<leader>c", group = "[C]ode", mode = { "n", "x" } },
+      { "<leader>d", group = "[D]ocument" },
+      { "<leader>r", group = "[R]ename" },
+      { "<leader>s", group = "[S]earch" },
+      { "<leader>w", group = "[W]orkspace" },
+      { "<leader>t", group = "[T]oggle" },
+      { "<leader>h", group = "Git [H]unk", mode = { "n", "v" } },
+      { "<leader>p", group = "Custom", mode = { "n" } },
+      { "<leader><C-s>", group = "[S]ave Operations", mode = { "n" } },
     },
   },
 }
