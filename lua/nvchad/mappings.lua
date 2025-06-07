130d4d75ce3b625deee0ec61f1fea1cdafab6a90
MillerTKG <cjrdm.mt@gmail.com>
2025-06-07 12:05:35 -0400

    test

--- lua/nvchad/mappings.lua
+++ lua/nvchad/mappings.lua
@@ -1,5 +1,7 @@
 local map = vim.keymap.set
 
+--test
+
 map("i", "<C-b>", "<ESC>^i", { desc = "move beginning of line" })
 map("i", "<C-e>", "<End>", { desc = "move end of line" })
 map("i", "<C-h>", "<Left>", { desc = "move left" })
