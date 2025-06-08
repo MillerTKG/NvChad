cba21cd6dc4b1bd3a3c653b6dcd7acd6184a61e5
MillerTKG <cjrdm.mt@gmail.com>
2025-06-08 14:24:35 -0400

    editing config

--- lua/nvchad/configs/whichkey.lua
+++ lua/nvchad/configs/whichkey.lua
@@ -1,21 +1,20 @@
-dofile(vim.g.base46_cache .. "whichkey")
+dofile(vim.g.base46_cache .. 'whichkey')
 
-return {
-  delay = 0,
-  triggers = {
-    { "<auto>", mode = "nixsotc" },
-    { "s", mode = { "n", "v" } },
-    -- { "a", mode = { "n", "v" } },
-  },
-  spec = {
-    -- { "<leader>c", group = "[C]ode", mode = { "n", "x" } },
-    -- { "<leader>d", group = "[D]ocument" },
-    -- { "<leader>r", group = "[R]ename" },
-    { "<leader>s", group = "[S]earch" },
-    -- { "<leader>w", group = "[W]orkspace" },
-    -- { "<leader>t", group = "[T]oggle" },
-    -- { "<leader>h", group = "Git [H]unk", mode = { "n", "v" } },
-    { "<leader>p", group = "Custom", mode = { "n" } },
-    -- { "<leader><C-s>", group = "[S]ave Operations", mode = { "n" } },
-  },
-}
+-- return {
+--   delay = 0,
+--   triggers = {
+--     { '<auto>', mode = 'nixsotc' },
+--     { 's', mode = { 'n', 'v' } },
+--   },
+--   spec = {
+--     -- { "<leader>c", group = "[C]ode", mode = { "n", "x" } },
+--     -- { "<leader>d", group = "[D]ocument" },
+--     -- { "<leader>r", group = "[R]ename" },
+--     { '<leader>s', group = '[S]earch' },
+--     -- { "<leader>w", group = "[W]orkspace" },
+--     -- { "<leader>t", group = "[T]oggle" },
+--     -- { "<leader>h", group = "Git [H]unk", mode = { "n", "v" } },
+--     { '<leader>p', group = 'Custom', mode = { 'n' } },
+--     -- { "<leader><C-s>", group = "[S]ave Operations", mode = { "n" } },
+--   },
+-- }
